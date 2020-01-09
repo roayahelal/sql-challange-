fb7082b557bbebe7d4815e676eae9b8e3a35faac
roayahelal <56054235+roayahelal@users.noreply.github.com>
2020-01-08 23:09:35 -0500

    update

--- Employee_SQL_Database/Queries.sql
+++ Employee_SQL_Database/Queries.sql
@@ -20,6 +20,13 @@ FROM dept_emp
 INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
 INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
 ;
+-- RH note: since employees are changing roles over time, adding more to the query to specify current role:
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+INNER JOIN departments ON dept_emp.dept_no = departments.dept_no
+INNER JOIN employees ON dept_emp.emp_no = employees.emp_no
+WHERE dept_emp.to_date = '9999-01-01'
+;
 
 --- 5.List all employees whose first name is "Hercules" and last names begin with "B."
 SELECT first_name, last_name FROM employees 
